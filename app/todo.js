1ef86cdb68b6396bd46308e515d2d749dade56c6
James Donlan <jacketlizard@live.com>
2018-02-19 15:01:14 -0800

    methods and looping

--- app/todo.js
+++ app/todo.js
@@ -1,11 +1,20 @@
 var Task = /** @class */ (function () {
-    function Task(descriptionParameter, priorityParameter) {
+    function Task(description, priority) {
+        this.description = description;
+        this.priority = priority;
         this.done = false;
-        this.description = descriptionParameter;
-        this.priority = priorityParameter;
     }
+    Task.prototype.markDone = function () {
+        this.done = true;
+    };
     return Task;
 }());
 var tasks = [];
 tasks.push(new Task('Do the dishes', 'Medium'));
-console.log(tasks);
+tasks.push(new Task('Buy chocolate', 'Low'));
+tasks.push(new Task('Do laundry', 'High'));
+tasks[0].markDone();
+for (var _i = 0, tasks_1 = tasks; _i < tasks_1.length; _i++) {
+    var task = tasks_1[_i];
+    console.log(task);
+}
